817d38ace32df1edd1bc667c3ac1f6c6e8291830
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-03-12 11:40:11 +0500

    feat: made the add_task method to add tasks to the storage and view_task method to display the tasks in a formatted way.

--- src/task_manager.py
+++ src/task_manager.py
@@ -1,22 +1,37 @@
+from task import Task
 class TaskManager:
 
+    def __init__(self):
+        self.storage = {}
 
-    @classmethod
-    def storage(cls, no , Task, priority, status):
-        record = {no: {1 : Task, 2: priority, 3:status  }}
 
-    def add_task(self):
-        title = input("Enter Task tltle: ")
-        priority = input("Enter Task priority (Low/Midium/High): ")
-        Task(title, priority )
+    def add_task(self,task, priority):
+            task = Task(task,priority)
+            no = len(self.storage) + 1
+
+            self.storage[no] = [task.title,task.priority,task.completed]
+
+
 
     def view_task(self):
+        for key , value in self.storage.items():
+             print(f"{key}. {value[0]} [{value[1]}] {'✔' if value[2] else '❌'}")
+
+    def complete_task(self):
+        pass
+
+    def delete_task(self):
         pass
 
 
 
+t = TaskManager()
+print(t.storage)
 
-
+t.add_task("do python", "high")
+t.add_task("do cpp", "low")
+t.view_task()
+# print(t.view_task())
 
 
 
