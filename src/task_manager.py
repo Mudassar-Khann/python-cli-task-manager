4dc3732ffff150b9bd4dc52eea944a4835fa203b
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-03-12 12:38:05 +0500

    feat: Added complete_task method to mark tasks as completed and updated view_task to handle empty task list.

--- src/task_manager.py
+++ src/task_manager.py
@@ -14,24 +14,42 @@ class TaskManager:
 
 
     def view_task(self):
-        for key , value in self.storage.items():
-             print(f"{key}. {value[0]} [{value[1]}] {'✔' if value[2] else '❌'}")
+        if len(self.storage) == 0:
+             print("Task list is empty")
+        else:
+            for key , value in self.storage.items():
+                print(f"{key}. {value[0]} [{value[1]}] {'✔' if value[2] else '❌'}")
 
     def complete_task(self):
-        pass
+         no = int(input("Enter Task no to complete: "))
+         if  no > len(self.storage) or no < 0:
+              print("Invalid no")
+         else:
+              self.storage[no][2] = True
+              print("Task marked as completed.")
+
+
 
     def delete_task(self):
         pass
 
 
+def main():
+    t = TaskManager()
+    # print(t.storage)
 
-t = TaskManager()
-print(t.storage)
 
-t.add_task("do python", "high")
-t.add_task("do cpp", "low")
-t.view_task()
-# print(t.view_task())
+
+    t.add_task("do python", "high")
+    t.add_task("do cpp", "low")
+    t.view_task()
+
+    t.complete_task()
+
+    t.view_task()
+
+if __name__ == "__main__":
+     main()
 
 
 
