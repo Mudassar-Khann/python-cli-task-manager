838f2b5523a954fa9895551a816bf640c991ea38
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-03-12 13:17:01 +0500

    feat: Add CLI menu for Task Manager application in main.py

--- src/main.py
+++ src/main.py
@@ -14,11 +14,28 @@ while True:
 
                 """)
 
-    user_choice = input("Select Option: ")
+    user_choice = int(input("Select Option: "))
 
 
-    if user_choice == "5":
+    if user_choice == 5:
         print("Closing program.....")
+        print("Godbye!")
         break
+
+    if user_choice == 1:
+        pick_title = input("Enter task title: ")
+        pick_priority = input("Enter priority (High/Medium/Low): ").strip().capitalize()
+        TaskManager.add_task(pick_title,pick_priority)
+
+    elif user_choice == 2:
+        TaskManager.view_task()
+
+    elif user_choice == 3:
+        TaskManager.complete_task()
+
+    elif user_choice == 4:
+        TaskManager.delete_task()
+
+
 
 
