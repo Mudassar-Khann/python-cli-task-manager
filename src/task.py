2066adb3d007c994d663f79fc7a8220e8fedcd99
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-03-17 20:53:56 +0500

    feat: formatting and type hints for Task class

--- src/task.py
+++ src/task.py
@@ -1,12 +1,15 @@
 class Task:
-    def __init__(self, title, priority, status=False):
+    """
+    Represents a single task
+    """
+
+    def __init__(self, title: str, priority: str, status: bool = False):
         self.title = title
         self.priority = priority
         self.completed = status
 
     def mark_complete(self):
+
         self.completed = True
 
-
-
-
+   
