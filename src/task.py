4634a24c88535b0ba6fd6d14e60827298789d97b
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-03-17 09:57:12 +0500

    feat: add parameter to set task status on initialization

--- src/task.py
+++ src/task.py
@@ -1,8 +1,8 @@
 class Task:
-    def __init__(self, title, priority):
+    def __init__(self, title, priority, status=False):
         self.title = title
         self.priority = priority
-        self.completed = False
+        self.completed = status
 
     def mark_complete(self):
         self.completed = True
